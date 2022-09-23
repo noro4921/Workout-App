12a7b688aac93a196998108679dc74f46e74c420
Reuben Khadka <noro4921@gmail.com>
2022-09-23 18:54:16 +1000

    Added the Exercises DB API

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Stack, Box, Button, Typography, TextField } from "@mui/material";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const SearchExercises = () => {
-  return <div>SearchExercises</div>;
+  const [search, setSearch] = useState("");
+
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      );
+
+      console.log(exercisesData);
+    }
+  };
+
+  return (
+    <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+      <Typography
+        fontWeight={700}
+        sx={{
+          fontSize: { lg: "44px", xs: "30px" },
+        }}
+        mb="50px"
+        textAlign="center"
+      >
+        Awesome Exercises <br /> you should know
+      </Typography>
+      <Box position="relative" mb="76px">
+        <TextField
+          sx={{
+            input: { fontWeight: "700", border: "none", borderRadius: "5px" },
+            width: { lg: "800px", xs: "350px" },
+            backgroundColor: "#ffff",
+            borderRadius: "40px",
+          }}
+          height="76px"
+          value={search}
+          onChange={(event) => setSearch(event.target.value.toLowerCase())}
+          placeholder="Search Exercises"
+          type="text"
+        />
+        <Button
+          className="search-btn"
+          sx={{
+            backgroundColor: "#ff2625",
+            color: "#fff",
+            textTransform: "none",
+            width: {
+              lg: "175px",
+              xs: "80px",
+            },
+            fontSize: {
+              lg: "20px",
+              xs: " 14px",
+            },
+            height: "56px",
+            position: "absolute",
+            right: "0",
+          }}
+          onClick={handleSearch}
+        >
+          Search
+        </Button>
+      </Box>
+    </Stack>
+  );
 };
 
 export default SearchExercises;
